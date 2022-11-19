be4de795fc9334e1a613f902ef6d707ba21b28c0
PavelBakhtin <pavelbakhtin88@gmail.com>
2022-11-19 23:15:32 +0200

    Update Filter.jsx

--- src/components/Phonebook/Filter/Filter.jsx
+++ src/components/Phonebook/Filter/Filter.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { setFilter } from 'components/redux/filterSlice';
 import { StyledInput } from '../ContactForm/ContactsForm.styled';
-import { flex, marginBottom } from 'styled-system';
 
 export const Filter = () => {
   const dispatch = useDispatch();
@@ -10,7 +9,12 @@ export const Filter = () => {
   };
   return (
     <>
-      <label htmlFor="filter">Find contacts by name</label>
+      <label
+        style={{ marginBottom: 10, display: 'inline-block' }}
+        htmlFor="filter"
+      >
+        Find contacts by name
+      </label>
       <StyledInput type="text" name="filter" onChange={onFilter} />
     </>
   );
